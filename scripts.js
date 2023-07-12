ac909ba60d32fbd7aea0866a894da0309acaa6a1
Francisco Darlan <118319737+FranciscoDarlan@users.noreply.github.com>
2023-07-11 21:25:30 -0300

    descomentando passo e res no  scripts.js
    variavel passo e res estava comentado

--- scripts.js
+++ scripts.js
@@ -1,12 +1,13 @@
 function contador() {
     const ini = document.getElementById('txti')
     const fim = document.getElementById('txtf')
-    /*
+    
     const passo = document.querySelector('p#txtp')
 
     const res = document.getElementById('res')
 
-*/
+
+    
     if (ini.value.length == 1 || fim.value.length == 0 || passo.value.length == 0) {
         alert('ERRO verifique os dados e tente novamente!')
     } else {
